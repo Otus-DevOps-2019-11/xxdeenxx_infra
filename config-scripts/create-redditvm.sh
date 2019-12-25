9b6af8919d73a047e4fc4df1c8c9cad91dada3fc
d_narezhnev <d.narezhnev@weplay.tv>
2019-12-25 09:25:15 +0200

    added packer scripts

---
+++ config-scripts/create-redditvm.sh
@@ -0,0 +1,4 @@
+gcloud compute instances create reddit-app\
+  --image-family reddit-full \
+  --tags puma-server \
+  --restart-on-failure
--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -5,9 +5,9 @@ wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add 
 
 bash -c 'echo deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 
-apt update
+apt-get update
 
-apt install -y mongodb-org
+apt-get install -y mongodb-org
 
 systemctl start mongod
 
