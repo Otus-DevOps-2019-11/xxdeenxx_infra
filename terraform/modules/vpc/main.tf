ea6eaded066c7d070a5f9a8a4ad5ddf8f527a647
d_narezhnev <d.narezhnev@weplay.tv>
2020-01-27 18:43:30 +0200

    add ansible-3

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -11,3 +11,17 @@ resource "google_compute_firewall" "firewall_ssh" {
     "reddit-db-${var.env}"
   ]
 }
+
+resource "google_compute_firewall" "firewall_http" {
+  name    = "allow-http-${var.env}"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges = var.source_ranges
+  target_tags = [
+    "reddit-app-${var.env}",
+    "reddit-db-${var.env}"
+  ]
+}
